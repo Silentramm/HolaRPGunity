02099eb0d201b1b469c5c6a85deeebc71bd87f63
Richard <rst656@hotmail.com>
2023-08-28 20:40:57 -0600

    Generar script Player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    //Velocidad a la que se tiene que mover el personaje
+    public float speed= 4.0f; //Para mover a una velocidad constante
+
+    //Guardar constantes que controlen el movimiento horzontal y vertical
+    private const string horizontal = "Horizontal";
+    private const string vertical = "Vertical";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame (El update se llama una vez por frame
+    void Update()
+    {
+        //Interprestar sentencias de control s = v*t espacio es igual a velocidad por tiempo
+        
+       
+    }
+}
